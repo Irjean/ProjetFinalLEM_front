73d07eb03cfbb3d5fa0f2175fb0bbd35d82e8b50
Haojing XU <leslie.hj.xu.design@gmail.com>
2023-11-27 20:05:32 +0100

    survey component structure

---
+++ src/components/survey/TextQuestion.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h2>{{ question.title }}</h2>
+      <textarea v-model="textAnswer" :maxlength="question.maxLength"></textarea>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['question'],
+    data() {
+      return {
+        textAnswer: '',
+      };
+    },
+  };
+  </script>
--- src/pages/SurveyPage.vue
+++ src/pages/SurveyPage.vue
@@ -1,22 +1,72 @@
 <template>
     <div>
       <h1>Page de Sondage</h1>
-      <SurveyForm />
-      <!-- Autres composants de sondage ici -->
+      
+      <component
+        v-for="(question,index) in questions"
+        :key="index"
+        :is="getQuestionComponent(question.type)"
+        :question="question"
+      />
+      <button @click="submitSurvey">Envoyer</button>
     </div>
 </template>
   
 <script>
-  import SurveyForm from '@/components/SurveyForm.vue';
-  import NavBar from '../components/NavBar.vue';
+import MultipleChoiceQuestion from '@/components/survey/MultipleChoiceQuestion.vue';
+import TextQuestion from '@/components/survey/TextQuestion.vue';
+import NumericQuestion from '@/components/survey/NumericQuestion.vue';
+import axios from 'axios';
+
   
   export default {
     components: {
-      SurveyForm,
-      NavBar
-      // Autres composants de sondage à importer ici
+      MultipleChoiceQuestion,
+      TextQuestion,
+      NumericQuestion,
     },
-    // Logique du composant
+
+    data(){
+      return {
+        loading: true,
+        questions: [],
+      };
+    },
+
+    mounted(){
+      this.fetchQuestions();
+    },
+
+    methods: {
+      fetchQuestions(){
+        axios.get('/api/questions')
+        .then(response => {
+          this.questoins = response.data.questions;
+          this.loading = false;
+        })
+        .catch(error => {
+          console.error('Erreur lors de la récupération des questions du sondage', error);
+        });
+      },
+
+      submitSurvey() {
+      // Logique pour traiter les réponses du sondage
+      // Peut inclure l'envoi des réponses à l'API lorsque celle-ci sera prête
+      // Redirection vers la page des résultats, etc.
+      },
+
+      getQuestionComponent(type) {
+        // Retourne le composant approprié en fonction du type de question
+        switch (type) {
+          case 'A':
+            return 'MultipleChoiceQuestion';
+          case 'B':
+            return 'TextQuestion';
+          case 'C':
+            return 'NumericQuestion';
+        }
+      },
+    },
   };
 </script>
   
