122adeebf91a2be3fa7110ffc6c3227fb1648f3e
Irjean <c.spileers@outlook.fr>
2024-01-26 16:42:31 +0100

    added analytics

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import { inject } from "@vercel/analytics";
 
 const pinia = createPinia()
 const app = createApp(App);
+inject();
 app.use(pinia);
 app.use(router);
 app.mount('#app');
